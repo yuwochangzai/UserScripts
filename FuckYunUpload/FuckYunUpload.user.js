2dfce4740a3fbc53ee8df3a47f8546a0b261b4fb
yuwochangzai <hujun20080813@gmail.com>
2014-03-20 23:18:41 +0800

    1、实现跳转到d页面后光标定位到验证码输入框输入验证码后回车提交在弹出的下载框点击下载操作流程

--- FuckYunUpload/FuckYunUpload.user.js
+++ FuckYunUpload/FuckYunUpload.user.js
@@ -3,7 +3,7 @@
 // @namespace   UserScripts
 // @description FuckYunUpload
 // @include     http://www.yunupload.net/*
-// @version     1
+// @version     0.1
 // @grant       none
 // @require     http://code.jquery.com/jquery-1.11.0.min.js
 // ==/UserScript==
@@ -14,26 +14,70 @@ $(function(){
 
 function switchMethod(){
     var pathName = location.pathname;
-    console.log(pathName);
     if(pathName.indexOf('/f/')>-1){//f目录
         gotoDownLoadPage();
     }
     else if (pathName.indexOf('/d/') > -1) {//d目录
-        //$('iframe').load(function () {
-        //    clearPageRubbish();
-        //});
-        //popupYmPrompt();
-        setTimeout(function () {
-            clearPageRubbish();
-            //popupYmPrompt();
-            //downLoadFile();
-        }, 1000);
-        //popup();
+        console.log(location);
+        handleDPage();
     }
 }
-//跳转到下载页面
+
+/**
+ * d页面处理
+ */
+function handleDPage()
+{
+    setTimeout(function () {
+        clearPageRubbish();
+        var hint = '请不要点击任何地方，直接键盘输入验证码后回车，再点击弹出框中的下载链接。否则会疯狂地弹出广告！';
+        //$('#yzm').append("<div style='background-color:yellow;width:100%;height:40px;position:relative;top:0;left:0;color:red;font-size:14px;font-weight:bold;'>请不要点击任何地方，直接键盘输入验证码后回车，再点击弹出框中的下载链接。否则会疯狂地弹出广告！</div>");
+        $('.downqu span:first font').html(hint);
+    }, 1000);   
+    $('#code').focus();//焦点聚焦到验证码文本框，此时鼠标不要点击任何位置，直接输入验证码后回车提交。
+    $('#code').keyup(function (event) {
+        if (event.keyCode == 13) {
+            clickDownbtn();
+        }
+    });
+}
+
+/**
+ * 模拟点击下载按钮下载
+ */
+function clickDownbtn()
+{
+    setTimeout(function () {
+        if ($('#downbtn').is(":visible")) {
+            $('#downbtn').click();
+            //clickDownsALabel();
+        }
+        else {
+            arguments.callee();
+        }
+    }, 1000);
+}
+
+/**
+ * 模拟点击弹出的a标签
+ */
+function clickDownsALabel() {
+    setTimeout(function () {
+        var $frameshowdown = $(window.frames['show_down'].document);
+        console.log($frameshowdown);
+        if ($frameshowdown.length > 0 && $frameshowdown.find('#downs').length > 0) {
+            $frameshowdown.find('#downs').click();
+        }
+        else {
+            arguments.callee();
+        }
+    }, 1000);
+}
+
+/**
+ * 跳转到下载页面
+ */
 function gotoDownLoadPage() {
-    clearInterval(timer1);
         redirectdown();//调用原页面函数，位于: yunupload.net/f/...html  http://www.yunupload.net/f/bf00e3ff82acc83c.html  
         alert('马上跳转到下载页面..');
 }
@@ -41,69 +85,28 @@ function gotoDownLoadPage() {
  * 清除页面垃圾
  */
 function clearPageRubbish() {
+    $('iframe').remove();
     $('a').remove();    
-    console.log($('body div:last').attr('style'));
-    //$('body div:last').remove();
-    removeMask();
-    console.log($('a div:last').attr('href'));
+    $('body div:last').remove();
+    //removeMask();
     $('body a:last').remove();
     $('body').unbind();
-    //$('.downqu').css('z-index', 2147483647);
-    $('.downqu').mouseover(function () {
-        console.log('mouseover');
-        $('body div:last').remove();
-    });
-    //var downqu = $('.downqu').html();
-    //console.log(downqu);
-    //$('body').html(downqu);
 }
-
+/**
+ * 定时移除遮照
+ */
 function removeMask() {
     var $lastMask = $('body div:last');
     var zindex = $lastMask.css('z-index');
     if (zindex > 1000) {
         //$lastMask.remove();
     }
-    $('body').unbind();
     setTimeout(function () {
         removeMask();
     }, 500);
 }
 
-function popup() {
-    var div = "<div id='divPopup' style='position:fixed;width:200px;height:200px;top:300px;left:200px;z-index:2147483647;background-color:red;'></div>";
-    $('body').append(div);
-}
 
-//弹出ymPrompt弹出框（没办法只好用自己的弹出框盖过它们）
-function popupYmPrompt()
-{
-    //$("<link>")
-    //.attr({
-    //    rel: "stylesheet",
-    //    type: "text/css",
-    //    href: "ymPrompt.css"
-    //}).appendTo("head");
-
-    var popupHtml = '<div><label>输入验证码：</label><input type="text"/></div>';
-    setTimeout(function () {
-        ymPrompt.win({ message: popupHtml, width: 200, height: 200, msgCls: '', showMask: true, title: '输入验证码' });
-        $('.ym-window').css({
-            'z-index': 20000000,
-            'background-color': 'white'
-        });
-        $('.ym-body').css('z-index', 20000200);
-        $('*').unbind('click');
-    },1000);
-}
-function getInput()
-{
-    var code = $('#myInput').val();
-    console.log(code);
-    $('#code').val($('#myInput').val());
-    $('.submit[name="Submit"]').click();
-    ymPrompt.close();
-}
 
 //真正下载弹出框
 function downLoadFile()
